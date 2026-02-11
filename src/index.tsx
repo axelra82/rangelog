3e0207f1482031ead9a6004b70d1af717aaf4d0f
Axel <986301+axelra82@users.noreply.github.com>
2026-02-11 18:21:25 +0100

    fix: block route and app from `/_/`.
    We need to be able to access `/_/` for pocketbase admin.

--- src/index.tsx
+++ src/index.tsx
@@ -7,57 +7,59 @@ import { ColorMode } from "./types";
 
 import { dashboardRoutes } from "./routes";
 
-const STORAGE_KEY = "color-mode";
+if (!window.location.pathname.startsWith("/_/")) {
+	const STORAGE_KEY = "color-mode";
 
-const savedMode = (localStorage.getItem(STORAGE_KEY) as ColorMode) ?? ColorMode.SYSTEM;
+	const savedMode = (localStorage.getItem(STORAGE_KEY) as ColorMode) ?? ColorMode.SYSTEM;
 
 
-const getSystemMode = (): ColorMode.LIGHT | ColorMode.DARK => {
-	if (typeof window === "undefined" || !window.matchMedia) {
-		return ColorMode.DARK;
-	}
-
-	return window.matchMedia("(prefers-color-scheme: dark)").matches
-		? ColorMode.DARK
-		: ColorMode.LIGHT;
-}
-
-const resolveMode = (mode: ColorMode): ColorMode.LIGHT | ColorMode.DARK => {
-	if (mode === ColorMode.SYSTEM) {
-		return getSystemMode();
-	}
-
-	return mode;
-}
-
-render(
-	() => {
-		const [mode, setMode] = createSignal<ColorMode>(savedMode);
-		createEffect(() => {
-			localStorage.setItem(STORAGE_KEY, mode());
-		});
-
-		if (window.matchMedia) {
-			const media = window.matchMedia("(prefers-color-scheme: dark)");
-			media.addEventListener("change", () => {
-				if (mode() === ColorMode.SYSTEM) {
-					setMode(ColorMode.SYSTEM);
-				}
-			});
+	const getSystemMode = (): ColorMode.LIGHT | ColorMode.DARK => {
+		if (typeof window === "undefined" || !window.matchMedia) {
+			return ColorMode.DARK;
 		}
 
-		return (
-			<ThemeProvider theme={createTheme({
-				palette: {
-					mode: resolveMode(mode()),
-				},
-			})}>
-				<CssBaseline />
-				<Router root={(props) => <App colorMode={mode} setColorMode={setMode} {...props} />}>
-					{dashboardRoutes}
-				</Router>
-			</ThemeProvider>
-		)
-	},
-	document.getElementById("root") as HTMLElement
-);
+		return window.matchMedia("(prefers-color-scheme: dark)").matches
+			? ColorMode.DARK
+			: ColorMode.LIGHT;
+	}
+
+	const resolveMode = (mode: ColorMode): ColorMode.LIGHT | ColorMode.DARK => {
+		if (mode === ColorMode.SYSTEM) {
+			return getSystemMode();
+		}
+
+		return mode;
+	}
+
+	render(
+		() => {
+			const [mode, setMode] = createSignal<ColorMode>(savedMode);
+			createEffect(() => {
+				localStorage.setItem(STORAGE_KEY, mode());
+			});
+
+			if (window.matchMedia) {
+				const media = window.matchMedia("(prefers-color-scheme: dark)");
+				media.addEventListener("change", () => {
+					if (mode() === ColorMode.SYSTEM) {
+						setMode(ColorMode.SYSTEM);
+					}
+				});
+			}
+
+			return (
+				<ThemeProvider theme={createTheme({
+					palette: {
+						mode: resolveMode(mode()),
+					},
+				})}>
+					<CssBaseline />
+					<Router root={(props) => <App colorMode={mode} setColorMode={setMode} {...props} />}>
+						{dashboardRoutes}
+					</Router>
+				</ThemeProvider>
+			)
+		},
+		document.getElementById("root") as HTMLElement
+	);
+}
