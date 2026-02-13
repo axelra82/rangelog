798a2286219ad94e9043e2ef42d40da11a895bfd
Axel <986301+axelra82@users.noreply.github.com>
2026-02-13 17:06:49 +0100

    fix: update assets.

---
+++ pwa-assets.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig, minimal2023Preset as preset } from '@vite-pwa/assets-generator/config'
+
+export default defineConfig({
+	preset: {
+		...preset,
+		transparent: {
+			sizes: [64, 192, 512],
+			favicons: [[48, 'favicon.ico']],
+			padding: 0
+		},
+		maskable: {
+			sizes: [512],
+			padding: 0
+		},
+		apple: {
+			sizes: [180],
+			padding: 0
+		}
+	},
+	images: [
+		'public/logo.svg',
+	]
+})
