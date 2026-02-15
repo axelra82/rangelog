f467f13bc9115288ebda3ff6ccc96b73c0bd8bbb
Axel <986301+axelra82@users.noreply.github.com>
2026-02-15 02:30:47 +0100

    fix: `main` scroll.

--- src/components/layout/content.tsx
+++ src/components/layout/content.tsx
@@ -2,8 +2,11 @@ import { ParentComponent } from "solid-js";
 
 export const LayoutMainContent: ParentComponent = (props) => {
 	return (
-		<main class="flex-1 p-4 overflow-auto max-w-4xl w-full mx-auto">
-			{props.children}
+		<main class="flex-1 overflow-auto w-full">
+			<div class="p-4 max-w-4xl mx-auto">
+				{props.children}
+
+			</div>
 		</main>
 	);
 };
